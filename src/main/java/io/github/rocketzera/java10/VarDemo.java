0e8ab29f66fc7ca804aaa0dd554163c9fb0e572d
Edicarlos Barbosa <edicarlosbarbosa@gmail.com>
2019-06-25 22:04:43 -0300

    Add Examples Reactive Java With Flow API

--- src/main/java/io/github/rocketzera/java10/VarDemo.java
+++ src/main/java/io/github/rocketzera/java10/VarDemo.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class VarDemo {
     public static void main(String[] args) {
         // String name = "Jair Bolsonaro"
-        var name = "Jair Bolsonaro";
+        //var name = "Jair Bolsonaro";
 
         // List<String> names = new ArrayList<>();
-        var names = new ArrayList <String>();
+        //var names = new ArrayList <String>();
 
         // Stream<String> stream = names.stream();
-        var stream = names.stream();
+        //var stream = names.stream();
     }
 }
